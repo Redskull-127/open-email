b4ec31a35e7735a685df0c87b9acfaf9ec67b6f3
Redskull-127 <meerraja17@gmail.com>
2026-02-18 00:31:59 +0530

    upd

--- apps/demo/lib/actions/actions.ts
+++ apps/demo/lib/actions/actions.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail(email: string, subject: string, body: string) {
     try {
         const { data, error } = await resend.emails.send({
-            from: "Open Email <onboarding@resend.dev>",
+            from: "Open Email <test@demo.oemail.in>",
             to: email,
             subject: subject,
             html: body,
